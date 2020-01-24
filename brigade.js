dd1e21c6c544811f26eda04b9265fc511a1129a1
Mayur Kumar <mayur.kumar@lapr200.successivetech.com>
2020-01-24 14:55:45 +0530

    sad

--- brigade.js
+++ brigade.js
@@ -1,35 +1,30 @@
 const { events, Job } = require("brigadier");
 events.on("push", (e, project) => {
-    //console.log("Received push for commit " + e.revision.commit)
-    //var commit = e.revision.commit.substr(e.revision.commit.length -7);
     var commit = e.revision.commit.substring(0, 7);
-    var uid = project.secrets.uid;
-   // "passwd": "p.secrets.passwd"
-    
     var greeting = new Job("job1", "alpine:latest");
     //greeting.storage.enabled = true;
     greeting.tasks = [
     "echo Hello Pipeline",
-    `echo commit id is ${commit}`,
-    `echo ${uid}`
-    ]
+    `echo commit id is ${commit}`
+    ];
 
     var docker = new Job("job2", "docker:dind");
     docker.privileged = true;
     docker.env = {
     "DOCKER_DRIVER": "overlay",
+    "uid": "project.secrets.uid",
+    "passwd": "project.secrets.passwd"
     }
 
     docker.tasks = [
         "dockerd-entrypoint.sh &",
         "sleep 10",
-        "cd /src",
-        "ls -l",
         `echo ${uid}`,
+        "echo $passwd",
        // `echo ${passwd}`,
         //`docker build -t mayursuccessive/hellonode:${commit} .`,
        // "docker login -u $uid -p $passwd",
-	    "docker login success", 
+	    "echo docker login success", 
 	    //`docker push mayursuccessive/hellonode:${commit}`,
         "docker images"
         ];
