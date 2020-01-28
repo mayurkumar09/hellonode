8f4f4da41bce641966e0d57232f70d9c57c802b8
Mayur Kumar <mayur.kumar@lapr200.successivetech.com>
2020-01-28 10:57:22 +0530

    sdas

--- brigade.js
+++ brigade.js
@@ -2,33 +2,43 @@ const { events, Job } = require("brigadier");
 
 events.on("push", (e, project) => {
     var commit = e.revision.commit.substring(0, 7);
+    var dest = "/mnt/brigade/share/abc.text";
+    
     var greeting = new Job("job1", "alpine:latest");
-    //greeting.storage.enabled = true;
+    greeting.storage.enabled = true;
     greeting.tasks = [
     "echo Hello Pipeline",
-    `echo commit id is ${commit}`
+    `echo commit id is ${commit} >` + dest 
     ];
 
-    var docker = new Job("job2", "docker:dind");
-    docker.privileged = true;
-    docker.env = {
-    "DOCKER_DRIVER": "overlay",
+    var print = new Job("job1", "alpine:latest");
+    print.storage.enabled = true;
+    print.tasks = [
+
+    "cat" + dest
+    ];
+
+    // var docker = new Job("job2", "docker:dind");
+    // docker.privileged = true;
+    // docker.storage.enabled = true;
+    // docker.env = {
+    // "DOCKER_DRIVER": "overlay",
     
-    }
-    docker.tasks = [
-        "dockerd-entrypoint.sh &",
-        "sleep 20", 
-        "cd /src",
-        `docker build -t mayursuccessive/hellonode:${commit} .`,
-        "docker images",
-        `docker login -u ${project.secrets.uid} -p ${project.secrets.passwd}`,
-	    "echo docker login success", 
-	    `docker push mayursuccessive/hellonode:${commit}`,
-        "docker images"
-        ];
+    // }
+    // docker.tasks = [
+    //     "dockerd-entrypoint.sh &",
+    //     "sleep 20", 
+    //     "cd /src",
+    //     `docker build -t mayursuccessive/hellonode:${commit} .`,
+    //     "docker images",
+    //     `docker login -u ${project.secrets.uid} -p ${project.secrets.passwd}`,
+	//     "echo docker login success", 
+	//     `docker push mayursuccessive/hellonode:${commit}`,
+    //     "docker images"
+    //     ];
 
  
     greeting.run();
     docker.run();
-
+    print.run();
 });
