b52b2627653554517440d176086a9634fcc0d2e3
Mayur Kumar <mayur.kumar@lapr200.successivetech.com>
2020-01-30 12:41:53 +0530

    asdas

--- brigade.js
+++ brigade.js
@@ -1,53 +1,52 @@
-
 const { events, Job } = require("brigadier");
 
 events.on("push", (e, project) => {
     var commit = e.revision.commit.substring(0, 7);
-    var dest = "/mnt/brigade/share/abc.text";
+   // var dest = "/mnt/brigade/share/abc.text";
     
     var greeting = new Job("job1", "alpine:latest");
-    greeting.storage.enabled = true;
+   // greeting.storage.enabled = true;
     greeting.tasks = [
     "echo Hello Pipeline",
-    `echo commit id is ${commit} >` + dest 
+   // `echo commit id is ${commit} >` + dest 
     ];
-
-    var print = new Job("job3", "devth/helm:latest");
-    print.storage.enabled = true;
-    print.tasks = [
-    "helm version",
-    "echo helm it is",
-    "gcloud version",
-    "kubectl get pods"
-    
-    ];
-
-    // var docker = new Job("job2", "docker:dind");
-    // docker.privileged = true;
-    // docker.storage.enabled = true;
-    // docker.env = {
-    // "DOCKER_DRIVER": "overlay",
-    
-    // }
-    // docker.tasks = [
-    //     "dockerd-entrypoint.sh &",
-    //     "sleep 20", 
-    //     "cd /src",
-    //     `docker build -t mayursuccessive/hellonode:${commit} .`,
-    //     "docker images",
-    //     `docker login -u ${project.secrets.uid} -p ${project.secrets.passwd}`,
-	//     "echo docker login success", 
-	//     `docker push mayursuccessive/hellonode:${commit}`,
-    //     "docker images"
-    //     ];
 
  
-    // greeting.run();
-    // docker.run();
-    // print.run();
 
-    greeting.run().then(() => {
-    print.run()
-    })
+    var docker = new Job("job2", "docker:dind");
+    docker.privileged = true;
+    docker.storage.enabled = true;
+    docker.env = {
+    "DOCKER_DRIVER": "overlay",
+    
+    }
+    docker.tasks = [
+        "dockerd-entrypoint.sh &",
+        "sleep 20", 
+        "cd /src",
+       // `docker build -t mayursuccessive/hellonode:${commit} .`,
+        "docker images",
+       // `docker login -u ${project.secrets.uid} -p ${project.secrets.passwd}`,
+	    "echo docker login success", 
+	    //`docker push mayursuccessive/hellonode:${commit}`,
+        //"docker images"
+        ];
+    
+    // var deploy = new Job("job3", "devth/helm:latest");
+    // deploy.storage.enabled = true;
+    // deploy.tasks = [
+    // "cd /src",
+    // "ls -l",
+    // "kubectl create -f "
+    
+    // ];
+ 
+    greeting.run();
+    docker.run();
+   // deploy.run();
+
+    // greeting.run().then(() => {
+    // deploy.run()
+    // })
 
 });
