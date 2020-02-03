c2fe88058cc6c457f7c0155d30d2d75987fe7de5
Mayur Kumar <mayur.kumar@lapr200.successivetech.com>
2020-02-03 10:54:37 +0530

    asdas

--- brigade.js
+++ brigade.js
@@ -3,20 +3,28 @@ const { events, Job } = require("brigadier");
 events.on("push", () => {
  
      
-    var job = new Job("dockerbuild", "docker:dind");
-    job.privileged = true;
-    job.env = {
-    DOCKER_DRIVER: "overlay"
-    }
-    job.tasks = [
-        "dockerd &",
-        "sleep 10",
-        "cd /src",
-        "ls -l",
-//        "docker build -t mayursuccessive/image-processing:v1 .",
-        "docker images"
-        ];
+    // var job = new Job("dockerbuild", "docker:dind");
+    // job.privileged = true;
+    // job.env = {
+    // DOCKER_DRIVER: "overlay"
+    // }
+    // job.tasks = [
+    //     "dockerd &",
+    //     "sleep 10",
+    //     "cd /src",
+    //     "ls -l",
+    //     "docker build -t mayursuccessive/image-processing:v1 .",
+    //     "docker images"
+    //     ];
     
-    job.run();
+    var helm = new Job("helm", "alpine/helm");
+    helm.task = [
+       // "helm repo add https://github.com/mayurkumar09/helm.git"
+        "git version"
+    ];
 
+
+
+    //job.run();
+    helm.run();
 });
